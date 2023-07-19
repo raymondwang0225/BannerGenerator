213ad3254c03d17470a10bfc586aa609801a8ede
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 14:29:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,48 +29,68 @@ def fix_image(upload, position, background_color, text, banner_size, text_size, 
 
 # Streamlit App
 def main():
+    st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+    
     st.title("Banner Generator")
 
-    uploaded_file = st.file_uploader("上传图片", type=['jpg', 'jpeg', 'png'])
-
-    # 指定图片位置
-    banner_width = st.slider("Banner宽度", 100, 1000, 500)
-    banner_height = st.slider("Banner高度", 100, 1000, 200)
-
-    # 指定背景颜色
-    background_color = st.color_picker("选择背景颜色", "#ffffff")
-
-    # 指定Banner文字
-    text = st.text_input("输入Banner文字")
-
-    # 指定Banner文字大小
-    text_size = st.slider("文字大小", 8, 120, 24)
-
-    # 指定Banner文字颜色
-    text_color = st.color_picker("文字颜色", "#000000")
-
-    # 指定Banner文字位置
-    text_position_x = st.slider("文字位置 (X)", -banner_width, banner_width, 0)
-    text_position_y = st.slider("文字位置 (Y)", -banner_height, banner_height, 0)
-    text_position = (text_position_x, -text_position_y)
+    uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
 
     if uploaded_file is not None:
+        col1, col2,col3 = st.columns(3)
+        if col1:
+            st.subheader("Banner")
+            # 指定背景颜色
+            background_color = st.color_picker("Choose Background Color", "#ffffff")
+            # 指定图片位置
+            banner_width = st.slider("Banner Width", 100, 1000, 500)
+            banner_height = st.slider("Banner Height", 100, 1000, 200)
+            
+        if col2:
+            st.subheader("Upload Image")
+            # 根据banner_size调整position的最大值和最小值
+            position_x = st.slider("Image Position(X)", -banner_height, banner_width, 100)
+            position_y = st.slider("Image Position(Y)", -banner_height, banner_height, 50)
+            position = (position_x, -position_y)
+
+        if col3:
+            st.subheader("Text")
+            # 指定Banner文字
+            text = st.text_input("Input Banner Text")
+             # 指定Banner文字颜色
+            text_color = st.color_picker("Text Color", "#000000")
+
+            # 指定Banner文字大小
+            text_size = st.slider("Text Size", 8, 120, 24)
+
+            # 指定Banner文字位置
+            text_position_x = st.slider("Text Position(X)", -banner_width, banner_width, 0)
+            text_position_y = st.slider("Text Position(Y)", -banner_height, banner_height, 0)
+            text_position = (text_position_x, -text_position_y)
+
         # 指定Banner尺寸
         banner_size = (banner_width, banner_height)
 
         # 生成Banner图片
-        banner_image = fix_image(uploaded_file, (0, 0), background_color, text, banner_size, text_size, text_color, text_position)
+        banner_image = fix_image(uploaded_file, position, background_color, text, banner_size, text_size, text_color, text_position)
 
-        # 使用st.columns来排列元素
-        col1, col2 = st.columns(2)
-        col1.image(banner_image)
+        # 显示Banner图片
+        st.image(banner_image)
 
         # 下载完成的图片
         buffered = BytesIO()
         banner_image.save(buffered, format="PNG")
         img_str = base64.b64encode(buffered.getvalue()).decode()
         href = f'<a href="data:file/png;base64,{img_str}" download="banner.png">点击下载</a>'
-        col2.markdown(href, unsafe_allow_html=True)
+        st.markdown(href, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
