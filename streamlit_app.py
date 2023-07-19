827bfe48d65602e9e6a3979285f4347130264ae5
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 18:22:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,68 +2,59 @@ import streamlit as st
 import cv2
 import numpy as np
 
-def process_image(image, tolerance):
-    # 將圖片轉換為RGBA格式
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+def process_image(image, color_tolerance):
+    # 将图像转换为HSV颜色空间
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
-    # 將圖片轉換為灰度圖像
-    gray = cv2.cvtColor(image, cv2.COLOR_RGBA2GRAY)
+    # 计算图像中每个颜色的像素数
+    hist = cv2.calcHist([hsv_image], [0], None, [180], [0, 180])
     
-    # 執行二值化處理，將圖像轉換為二值圖像
-    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+    # 找到颜色频次最高的色相
+    dominant_color_hue = np.argmax(hist)
     
-    # 找到圖像中的輪廓
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # 根据色相和容差计算颜色范围
+    lower_color = np.array([dominant_color_hue - color_tolerance, 100, 100])
+    upper_color = np.array([dominant_color_hue + color_tolerance, 255, 255])
     
-    # 找到面積最大的輪廓
+    # 创建一个掩码来标记在颜色范围内的像素
+    mask = cv2.inRange(hsv_image, lower_color, upper_color)
+    
+    # 计算掩码中最大连通区域的面积和位置
+    _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     max_contour = max(contours, key=cv2.contourArea)
+    _, _, width, height = cv2.boundingRect(max_contour)
+    max_area = width * height
     
-    # 創建一個與原始圖像相同大小的遮罩
-    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    # 根据最大连通区域的位置创建一个新的掩码
+    new_mask = np.zeros_like(mask)
+    cv2.drawContours(new_mask, [max_contour], 0, (255), -1)
     
-    # 在遮罩上繪製面積最大的輪廓
-    cv2.drawContours(mask, [max_contour], -1, 255, thickness=cv2.FILLED)
-    
-    # 使用形態學操作對遮罩進行膨脹，填充輪廓周圍的區域
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-    mask = cv2.dilate(mask, kernel)
-    
-    # 應用遮罩以僅保留面積最大的區域
-    image = cv2.bitwise_and(image, image, mask=mask)
-    
-    # 計算各個顏色的像素數量
-    unique_colors, counts = np.unique(image.reshape(-1, 4), axis=0, return_counts=True)
-    
-    # 找到佔比最大的顏色
-    max_color = unique_colors[np.argmax(counts)]
-    
-    # 將該顏色設置為透明
-    mask = np.all(np.abs(image - max_color) <= tolerance, axis=2)
-    image[mask] = [0, 0, 0, 0]
+    # 将新的掩码应用于图像，将最大连通区域以外的像素设置为透明
+    image[new_mask == 0] = [0, 0, 0, 0]  # 设置透明像素的颜色
     
     return image
 
 def main():
-    st.title("圖片處理應用")
+    st.title("图像处理")
     
-    # 上傳圖片
-    uploaded_file = st.file_uploader("選擇一張圖片", type=["jpg", "jpeg", "png"])
-    
-    # 容忍度滑桿
-    tolerance = st.slider("容忍度", min_value=0, max_value=500, value=10)
+    # 创建一个上传文件的组件
+    uploaded_file = st.file_uploader("选择一张图片", type=["jpg", "jpeg", "png"])
     
     if uploaded_file is not None:
-        # 讀取上傳的圖片
-        image = np.array(cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1))
+        # 读取上传的图像
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
         
-        # 顯示原始圖片
-        #st.image(image, caption="原始圖片", use_column_width=True)
+        # 显示原始图像
+        #st.image(image, channels="BGR", caption="原始图像")
         
-        # 處理圖片，傳遞容忍度參數
-        processed_image = process_image(image, tolerance)
+        # 创建一个滑动条，用于控制容差
+        color_tolerance = st.slider("容差", 0, 50, 10)
         
-        # 顯示處理後的圖片
-        st.image(processed_image, caption="處理後的圖片", use_column_width=True)
+        # 处理图像
+        processed_image = process_image(image, color_tolerance)
+        
+        # 显示处理后的图像
+        st.image(processed_image, channels="BGR", caption="处理后的图像")
 
 if __name__ == "__main__":
     main()
