fe6134b7cb34b590ecc1e8b4676e5cbb55669bd6
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 13:07:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,14 @@ from PIL import Image
 from io import BytesIO
 import base64
 import numpy as np
+import tempfile
 
 def convert_image(img):
-    img_array = np.array(img)
-    buf = BytesIO()
-    np.save(buf, img_array)
-    byte_im = buf.getvalue()
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.close()
+    img.save(temp_file.name, format="PNG")
+    with open(temp_file.name, "rb") as file:
+        byte_im = file.read()
     return byte_im
 
 def fix_image(upload, position, background_color, text):
