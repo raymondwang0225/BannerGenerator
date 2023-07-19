bd871daa22cb59d538e3768b17ad24f0642bb14e
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 18:11:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,27 @@ def process_image(image, tolerance):
     # 將圖片轉換為RGBA格式
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
     
+    # 將圖片轉換為灰度圖像
+    gray = cv2.cvtColor(image, cv2.COLOR_RGBA2GRAY)
+    
+    # 執行二值化處理，將圖像轉換為二值圖像
+    _, binary = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+    
+    # 找到圖像中的輪廓
+    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # 找到區域面積最大的輪廓
+    max_contour = max(contours, key=cv2.contourArea)
+    
+    # 創建一個與原始圖像相同大小的遮罩
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    
+    # 在遮罩上繪製區域面積最大的輪廓
+    cv2.drawContours(mask, [max_contour], -1, 255, thickness=cv2.FILLED)
+    
+    # 應用遮罩以僅保留區域面積最大的區域
+    image = cv2.bitwise_and(image, image, mask=mask)
+    
     # 計算各個顏色的像素數量
     unique_colors, counts = np.unique(image.reshape(-1, 4), axis=0, return_counts=True)
     
