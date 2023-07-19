07430822ea29f42bda03ca552e4c27f2cbab2a39
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 15:28:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,23 @@
 import streamlit as st
-from rembg import remove
-from PIL import Image
+import cv2
 import numpy as np
+from PIL import Image
 
-def remove_background(image, background_color):
-    # 将图像转换为NumPy数组
-    image_array = np.array(image)
 
-    # 获取图像的宽度和高度
-    width, height = image.size
-
+def remove_background(image, mask):
     # 将图像转换为RGBA模式，以支持透明度
     image = image.convert("RGBA")
 
-    # 获取图像中的每个像素
-    data = image.getdata()
+    # 获取图像的像素数据
+    data = np.array(image)
 
-    # 创建一个新的像素列表，根据背景颜色去除背景
-    new_data = []
-    for item in data:
-        # 判断像素颜色是否与背景颜色完全匹配
-        if item[:3] == background_color:
-            # 将匹配的像素设置为透明
-            new_data.append((0, 0, 0, 0))
-        else:
-            new_data.append(item)
+    # 将分割的背景区域设置为透明
+    data[..., 3] = np.where(mask == 0, 0, 255)
 
     # 创建新的图像并返回
-    new_image = Image.new("RGBA", (width, height))
-    new_image.putdata(new_data)
-
+    new_image = Image.fromarray(data)
     return new_image
+
 
 # Streamlit App
 def main():
@@ -43,21 +30,46 @@ def main():
         # 读取上传的图像
         image = Image.open(uploaded_file)
 
-        # 显示图像和滴管工具
-        background_color = st.image(image, use_column_width=True, caption="使用滴管工具选择背景颜色")
-        background_color_picker = st.color_picker("选择背景颜色", value="#ffffff")
+        # 显示图像
+        st.image(image, caption="原始图像")
 
-        # 当选择颜色后执行背景去除操作
-        if st.button("去除背景"):
-            # 获取选择的背景颜色
-            selected_color = tuple(int(background_color_picker.lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
+        # 显示图像选择工具
+        st.subheader("选择背景区域")
+        selected_area = st.image(image, caption="选择背景区域", use_column_width=True, clamp=True)
 
-            # 去除背景
-            removed_background = remove_background(image, selected_color)
+        # 创建空的掩码图像
+        mask = np.zeros(image.size, dtype=np.uint8)
 
-            # 显示去除背景后的图像
-            st.subheader("去除背景后的图像")
-            st.image(removed_background)
+        # 设置鼠标事件处理函数
+        def mouse_callback(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                # 在掩码图像上绘制白色点
+                cv2.circle(mask, (x, y), 3, (255, 255, 255), -1)
+                selected_area.image(mask, caption="选择背景区域", use_column_width=True, clamp=True)
+
+        # 将图像转换为OpenCV格式
+        cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+        # 创建窗口并设置鼠标事件回调
+        cv2.namedWindow("Select Background")
+        cv2.setMouseCallback("Select Background", mouse_callback)
+
+        # 显示OpenCV窗口
+        cv2.imshow("Select Background", cv_image)
+
+        # 等待用户选择背景区域
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        # 运行图像分割算法，如分水岭算法或GrabCut算法，对选择的背景区域进行分割并获得分割结果的掩码
+
+        # 调用去除背景函数，将分割结果应用于图像
+        removed_background = remove_background(image, mask)
+
+        # 显示去除背景后的图像
+        st.subheader("去除背景后的图像")
+        st.image(removed_background)
+
 
 if __name__ == "__main__":
     main()
