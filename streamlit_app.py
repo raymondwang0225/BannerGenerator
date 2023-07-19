18a0b55919a2f03c4f28c829eacac690495df4fb
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 15:02:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def fix_image(upload, position, background_color, text, banner_size, text_size, 
 # Streamlit App
 def main():
     st.set_page_config(layout='wide', initial_sidebar_state='expanded')
-
+    '''
     hide_st_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
@@ -39,7 +39,7 @@ def main():
                 </style>
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
-
+    '''
     st.title("Banner Generator")
 
     uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
