90666566e0bcfe7c87eb655fd61e1b248217e2ed
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-19 16:34:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,123 @@
-import cv2
 import streamlit as st
-import numpy as np
-from PIL import Image
 from rembg import remove
-
-def brighten_image(image, amount):
-    img_bright = cv2.convertScaleAbs(image, beta=amount)
-    return img_bright
-
-
-def blur_image(image, amount):
-    blur_img = cv2.GaussianBlur(image, (11, 11), amount)
-    return blur_img
+from PIL import Image
+from io import BytesIO
+import base64
+from PIL import ImageDraw, ImageFont
+import cv2
+import numpy as np
 
 
-def enhance_details(img):
-    hdr = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
-    return hdr
+def bgremove3(myimage):
+    # BG Remover 3
+    myimage_hsv = cv2.cvtColor(myimage, cv2.COLOR_BGR2HSV)
+     
+    #Take S and remove any value that is less than half
+    s = myimage_hsv[:,:,1]
+    s = np.where(s < 127, 0, 1) # Any value below 127 will be excluded
+ 
+    # We increase the brightness of the image and then mod by 255
+    v = (myimage_hsv[:,:,2] + 127) % 255
+    v = np.where(v > 127, 1, 0)  # Any value above 127 will be part of our mask
+ 
+    # Combine our two masks based on S and V into a single "Foreground"
+    foreground = np.where(s+v > 0, 1, 0).astype(np.uint8)  #Casting back into 8bit integer
+ 
+    background = np.where(foreground==0,255,0).astype(np.uint8) # Invert foreground to get background in uint8
+    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)  # Convert background back into BGR space
+    foreground=cv2.bitwise_and(myimage,myimage,mask=foreground) # Apply our foreground map to original image
+    finalimage = background+foreground # Combine foreground and background
+ 
+    return finalimage
 
 
-def main_loop():
-    st.title("OpenCV Demo App")
-    st.subheader("This app allows you to play with Image filters!")
-    st.text("We use OpenCV and Streamlit for this demo")
+def fix_image(upload, position, background_color, text, banner_size, text_size, text_color, text_position):
+    image = Image.open(upload)
+    fixed = bgremove3(image)
 
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
+    # 缩放fixed图像至banner尺寸并保持比例
+    fixed.thumbnail(banner_size)
 
-    image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
-    if not image_file:
-        return None
+    # 创建 Banner 图片
+    banner_image = Image.new('RGBA', banner_size, background_color)
+    banner_image.paste(fixed, position, fixed)
 
-    original_image = Image.open(image_file)
-    original_image = np.array(original_image)
+    # 在 Banner 图片上添加文字
+    draw = ImageDraw.Draw(banner_image)
+    font = ImageFont.truetype("Pixels.ttf", text_size)
+    text_width, text_height = draw.textsize(text, font=font)
+    text_position_x = text_position[0] - text_width / 2
+    text_position_y = text_position[1] - text_height / 2
+    draw.text((text_position_x, text_position_y), text, fill=text_color, font=font)
 
-    processed_image = blur_image(original_image, blur_rate)
-    processed_image = brighten_image(processed_image, brightness_amount)
+    return banner_image
 
-    if apply_enhancement_filter:
-        processed_image = enhance_details(processed_image)
 
-    st.text("Original Image vs Processed Image")
-    st.image([original_image, processed_image])
-    st.image(remove(processed_image))
+# Streamlit App
+def main():
+    #st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-if __name__ == '__main__':
-    main_loop()
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
+    st.title("Banner Generator")
+
+    uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+
+    if uploaded_file is not None:
+        col1, col2,col3 = st.columns(3,gap="large")
+        with col1:
+            st.subheader("Banner")
+            # 指定背景颜色
+            background_color = st.color_picker("Choose Background Color", "#ffffff")
+            # 指定图片位置
+            banner_width = st.slider("Banner Width", 100, 1000, 500)
+            banner_height = st.slider("Banner Height", 100, 1000, 200)
+            
+        with col2:
+            st.subheader("Image")
+            # 根据banner_size调整position的最大值和最小值
+            position_x = st.slider("Image Position(X)", -banner_height, banner_width, 100)
+            position_y = st.slider("Image Position(Y)", -banner_height, banner_height, 50)
+            position = (position_x, -position_y)
+
+        with col3:
+            st.subheader("Text")
+            # 指定Banner文字
+            text = st.text_input("Input Banner Text")
+             # 指定Banner文字颜色
+            text_color = st.color_picker("Text Color", "#000000")
+
+            # 指定Banner文字大小
+            text_size = st.slider("Text Size", 8, 120, 24)
+
+            # 指定Banner文字位置
+            text_position_x = st.slider("Text Position(X)", -banner_width, banner_width, 0)
+            text_position_y = st.slider("Text Position(Y)", -banner_height, banner_height, 0)
+            text_position = (text_position_x, -text_position_y)
+
+        # 指定Banner尺寸
+        banner_size = (banner_width, banner_height)
+
+        # 生成Banner图片
+        banner_image = fix_image(uploaded_file, position, background_color, text, banner_size, text_size, text_color, text_position)
+
+        # 显示Banner图片
+        st.image(banner_image)
+
+        # 下载完成的图片
+        buffered = BytesIO()
+        banner_image.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+        href = f'<a href="data:file/png;base64,{img_str}" download="banner.png">点击下载</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
