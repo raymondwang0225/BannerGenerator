83c929696052600140048aba23221a58b9da495c
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-20 13:45:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,38 +43,39 @@ def fix_image(upload, position, background_color, text, banner_size, text_size, 
 
 # Streamlit App
 def main():
-    st.set_page_config(layout='centered', initial_sidebar_state='expanded')
+    st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
     st.title("Banner Generator")
 
     uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+    col1,col2 = st.columns([3,7])
+    with col1:
+        if uploaded_file is not None:
+            with st.expander("Banner Setting"):
+                # 指定背景顏色
+                background_color = st.color_picker("Choose Background Color", "#ffffff")
+                # 指定圖片位置
+                banner_width = st.slider("Banner Width", 100, 1500, 1500)
+                banner_height = st.slider("Banner Height", 100, 500, 500)
 
-    if uploaded_file is not None:
-        with st.expander("Banner Setting"):
-            # 指定背景顏色
-            background_color = st.color_picker("Choose Background Color", "#ffffff")
-            # 指定圖片位置
-            banner_width = st.slider("Banner Width", 100, 1500, 1500)
-            banner_height = st.slider("Banner Height", 100, 500, 500)
+            with st.expander("Image Setting"):
+                # 根據banner_size調整position的最大值和最小值
+                position_x = st.slider("Image Position(X)", -banner_height, banner_width, 100)
+                position_y = st.slider("Image Position(Y)", -banner_height, banner_height, 50)
+                position = (position_x, -position_y)
 
-        with st.expander("Image Setting"):
-            # 根據banner_size調整position的最大值和最小值
-            position_x = st.slider("Image Position(X)", -banner_height, banner_width, 100)
-            position_y = st.slider("Image Position(Y)", -banner_height, banner_height, 50)
-            position = (position_x, -position_y)
-
-        with st.expander("Text Setting"):
-            # 指定Banner文字
-            text = st.text_input("Input Banner Text", "Bitcoin Frogs")
-            # 指定Banner文字顏色
-            text_color = st.color_picker("Text Color", "#ffffff")
-            # 指定Banner文字大小
-            text_size = st.slider("Text Size", 8, 240, 120)
-            # 指定Banner文字位置
-            text_position_x = st.slider("Text Position(X)", -banner_width, banner_width, 0)
-            text_position_y = st.slider("Text Position(Y)", -banner_height, banner_height, 0)
-            text_position = (text_position_x, -text_position_y)
-
+            with st.expander("Text Setting"):
+                # 指定Banner文字
+                text = st.text_input("Input Banner Text", "Bitcoin Frogs")
+                # 指定Banner文字顏色
+                text_color = st.color_picker("Text Color", "#ffffff")
+                # 指定Banner文字大小
+                text_size = st.slider("Text Size", 8, 240, 120)
+                # 指定Banner文字位置
+                text_position_x = st.slider("Text Position(X)", -banner_width, banner_width, 0)
+                text_position_y = st.slider("Text Position(Y)", -banner_height, banner_height, 0)
+                text_position = (text_position_x, -text_position_y)
+    with col2:
         # 指定Banner尺寸
         banner_size = (banner_width, banner_height)
 
@@ -82,12 +83,13 @@ def main():
         processing_option = st.selectbox("Choose Processing Option", ("default", "customize"))
 
         if processing_option == "customize":
-            # Add sliders for custom settings
-            alpha_matting_custom = {
-                "foreground_threshold": st.slider("Alpha Matting Foreground Threshold", 0, 20, 9),
-                "background_threshold": st.slider("Alpha Matting Background Threshold", 0, 20, 3),
-                "erode_size": st.slider("Alpha Matting Erode Size", 0, 50, 17)
-            }
+            with st.expander("Remove Background Setting"):
+                # Add sliders for custom settings
+                alpha_matting_custom = {
+                    "foreground_threshold": st.slider("Alpha Matting Foreground Threshold", 0, 20, 9),
+                    "background_threshold": st.slider("Alpha Matting Background Threshold", 0, 20, 3),
+                    "erode_size": st.slider("Alpha Matting Erode Size", 0, 50, 17)
+                }
         else:
             alpha_matting_custom = None
 
