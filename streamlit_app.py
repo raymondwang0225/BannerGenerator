10c1d260485ddaec5376f1746008c62becc59893
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-20 12:48:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ from PIL import ImageDraw, ImageFont
 import time
 
 
-def fix_image(upload, position, background_color, text, banner_size, text_size, text_color, text_position,alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size,progress):
+def fix_image(upload, position, background_color, text, banner_size, text_size, text_color, text_position,progress):
     image = Image.open(upload)
-    fixed = remove(image, alpha_matting=True, alpha_matting_foreground_threshold=alpha_matting_foreground_threshold, alpha_matting_background_threshold=alpha_matting_background_threshold, alpha_matting_erode_size=alpha_matting_erode_size)
+    fixed = remove(image, alpha_matting=True, alpha_matting_foreground_threshold=9, alpha_matting_background_threshold=3, alpha_matting_erode_size=17)
 
     # 縮放fixed圖像至banner尺寸並保持比例
     fixed.thumbnail(banner_size)
@@ -94,15 +94,9 @@ def main():
         with st.spinner('Image processing, please wait...'):
             # 处理图片并显示进度
             # 生成Banner圖片
-            banner_image = fix_image(uploaded_file, position, background_color, text, banner_size, text_size, text_color, text_position, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size,progress_placeholder)
+            banner_image = fix_image(uploaded_file, position, background_color, text, banner_size, text_size, text_color, text_position,progress_placeholder)
         # 顯示Banner圖片
         st.image(banner_image)
-
-        with st.expander("Customized Setting"):
-            # 調整透明度分割的相關參數
-            alpha_matting_foreground_threshold = st.slider("Foreground Threshold", 0, 255, 270)
-            alpha_matting_background_threshold = st.slider("Background Threshold", 0, 255, 20)
-            alpha_matting_erode_size = st.slider("Erode Size", 0, 50, 11)
 
         # 下載完成的圖片
         buffered = BytesIO()
