b306dba823904310b7166b2a715dc5627935599c
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-20 10:05:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,9 +80,9 @@ def main():
         banner_size = (banner_width, banner_height)
 
         # 調整透明度分割的相關參數
-        alpha_matting_foreground_threshold = st.slider("Foreground Threshold", 0, 255, 270)
-        alpha_matting_background_threshold = st.slider("Background Threshold", 0, 255, 20)
-        alpha_matting_erode_size = st.slider("Erode Size", 0, 50, 11)
+        #alpha_matting_foreground_threshold = st.slider("Foreground Threshold", 0, 255, 270)
+        #alpha_matting_background_threshold = st.slider("Background Threshold", 0, 255, 20)
+        #alpha_matting_erode_size = st.slider("Erode Size", 0, 50, 11)
 
         # 生成Banner圖片
         banner_image = fix_image(uploaded_file, position, background_color, text, banner_size, text_size, text_color, text_position, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size)
